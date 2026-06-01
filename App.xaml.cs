ad6944f754cefef498cab31f82e674697e855a9d
Elena <elena-priester@t-online.de>
2026-06-01 17:51:06 +0200

    App.xaml.cs debug wieder entfernt

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,21 +4,4 @@ namespace TodoAppWPF;
 
 public partial class App : Application
 {
-    public App()
-    {
-        AppDomain.CurrentDomain.UnhandledException += (s, ex) =>
-        {
-            MessageBox.Show(ex.ExceptionObject.ToString(), "Kritischer Fehler");
-        };
-    }
-
-    protected override void OnStartup(StartupEventArgs e)
-    {
-        base.OnStartup(e);
-        DispatcherUnhandledException += (s, ex) =>
-        {
-            MessageBox.Show(ex.Exception.ToString(), "Fehler");
-            ex.Handled = true;
-        };
-    }
 }
